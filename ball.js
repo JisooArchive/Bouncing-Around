1090bc0eaf0823bb44fda444234b54d464756dd2
JisooArchive <jisoo.cheh@gmail.com>
2025-02-13 22:12:29 +0900

    Update ball.js

--- ball.js
+++ ball.js
@@ -3,6 +3,7 @@ export class Ball {
         this.radius = radius;
         this.vx = speed;
         this.vy = speed
+        
         const diameter = this.radius * 2
         this.x = diameter + (Math.random() * stageWidth - diameter)
         this.y = diameter + (Math.random() * stageHeight - diameter)
