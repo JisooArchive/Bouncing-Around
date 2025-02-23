af14e53204bf643a1c0edc4c1664570db3367d2f
JisooArchive <jisoo.cheh@gmail.com>
2025-02-23 21:23:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ class App {
     resize() {
         this.stageWidth = document.body.clientWidth;
         this.stageHeight = document.body.clientHeight
-
         this.canvas.width = this.stageWidth * 2;
         this.canvas.height = this.stageHeight * 2;
         
