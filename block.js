40029feb0c4a08927eb813b575a056a88094e270
JisooArchive <jisoo.cheh@gmail.com>
2025-02-23 21:22:44 +0900

    Update block.js

--- block.js
+++ block.js
@@ -2,7 +2,7 @@ export class Block {
     constructor(width, height, x, y) {
         this.width = width;
         this.height = height;
-        
+    
         this.x = x;
         this.y = y;
         this.maxX = width + x;
